a86d896bb5b33a83da27a5b6934a20afa382cb48
keerthichowdarysurampalli <keerthichowdary259@gmail.com>
2023-10-13 21:58:07 -0400

    jhfh

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
 import Image from 'next/image';
-import { UserButton } from "@clerk/nextjs";
 import Link from 'next/link';
-import Navigation from './component/Navigation';
 
 
 export default function Home() {
   return (
     <>
-  <Navigation />
+
       <div className='flex flex-col  h-fit'>
         <div className='flex flex-row h-full gap-4 bg-gray-100 px-16 py-8'>
           <div className='flex flex-col  h-full gap-6'>
             <h1 className='text-7xl font-bold'>
-              Elevating Patient Care with Ohms hello
+              Elevating Patient Care with Ohms
             </h1>
             <p>
               Ohms is a hospital management app that offers streamlined patient tracking, medication management, and pharmacy integration. With an intuitive user interface, Ohms provides a comprehensive solution for better healthcare management.
@@ -20,7 +18,7 @@ export default function Home() {
             <Link type="button" href={'/view-all-services'} className="text-gray-900 bg-white border w-fit border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" >View All Services</Link>
 
           </div>
-          <Image src={'/image.jpg'} alt='doctor and nure' width={600} height={600} className='rounded-xl'></Image>
+          <Image src={'/image.jpg'} alt='doctor and nure' width={600} height={600} className='rounded-xl shadow-2xl'></Image>
         </div>
       </div>
 
